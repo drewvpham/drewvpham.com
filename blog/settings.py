a3ad928489873f6f26b6b49df95470354fe95999
Drew Pham <drewvpham@gmail.com>
2020-09-05 20:15:49 -0500

    edit file save

--- blog/settings.py
+++ blog/settings.py
@@ -2,7 +2,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+FILE_UPLOAD_PERMISSIONS = 0o640
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
